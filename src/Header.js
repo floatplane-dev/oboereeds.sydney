c7927c46cf29d8011d0b92b69abaa21ec4879739
Richard <rv@floatplane.dev>
2020-05-03 17:13:14 +1000

    Add Header component

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Sydney Oboe Reeds</h1>
+        <nav>
+          <a>buying guide</a>
+          <a>contact</a>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
