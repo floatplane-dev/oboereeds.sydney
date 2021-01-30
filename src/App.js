4d93e96d6322447e74786f88f5f1254cb9f61905
Richard <rv@floatplane.dev>
2021-01-30 15:27:40 +1100

    debug height issue

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     window.addEventListener('resize', () => {
       // We execute the same script as before
       let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh1', `${vh}px`);
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
   }
 
