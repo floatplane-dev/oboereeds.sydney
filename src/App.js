9d3d8e264904dcd672b88803ba0e75ad13f7eb9d
Richard <rv@floatplane.dev>
2021-01-30 15:22:52 +1100

    debug height issue

--- src/App.js
+++ src/App.js
@@ -17,11 +17,14 @@ import Footer from "./Footer/";
 class App extends Component {
 
   componentDidMount() {
-  //   window.addEventListener('resize', () => {
+    // let vh2 = window.innerHeight * 0.01;
+    // document.documentElement.style.setProperty('--vh1', `${vh2}px`);
+
+    window.addEventListener('resize', () => {
       // We execute the same script as before
       let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-  //   });
+      document.documentElement.style.setProperty('--vh1', `${vh}px`);
+    });
   }
 
   render() {
